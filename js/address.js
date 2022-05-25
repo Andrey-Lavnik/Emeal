f63880ec64e115f467e3298cbbca703a4452fc64
PashkaSharl <82102965+PashkaSharl@users.noreply.github.com>
2022-05-25 18:11:19 +0300

    changed html js css

--- js/address.js
+++ js/address.js
@@ -58,3 +58,18 @@ const dedLine = '2022-05-13'
     }
   }
   setClock('.timer',dedLine)
+
+
+
+
+  const burger = document.querySelector('.header_menu') 
+const sidebar = document.querySelector('.sidebar')
+burger.addEventListener('click',()=>{ 
+    if(burger.classList.contains('active')){ 
+        burger.classList.remove('active')
+        sidebar.classList.remove('active')
+    }else{ 
+        burger.classList.add('active') 
+        sidebar.classList.add('active')
+    } 
+})
