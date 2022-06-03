acc5ffb976045c68ffce84a0ba833dbbc1e2370e
PashkaSharl <82102965+PashkaSharl@users.noreply.github.com>
2022-06-03 21:20:01 +0300

    change html/css

--- js/address.js
+++ js/address.js
@@ -25,44 +25,7 @@ closeModal2.addEventListener('click', () =>{
     modal2.classList.remove('active2')
 })
 
-
-const dedLine = '2022-05-13'
-  function getTimeRemaining(endTime){
-    const t = Date.parse(endTime) - Date.parse(new Date())
-    const days = Math.floor(t/(1000*60*60*24));
-    const hours = Math.floor((t/(1000*60*60))%24);
-    const minutes = Math.floor((t/(1000*60))%60);
-    const seconds = Math.floor((t/1000)%60);
-    return{
-      total: t,
-      days: days,
-      hours: hours,
-      minutes: minutes,
-      seconds: seconds
-    }
-  }
-  function setClock(selector,endTime){
-    const timer = document.querySelector(selector);
-    const days = timer.querySelector("#days");
-    const hours = timer.querySelector("#hours");
-    const minutes = timer.querySelector("#minutes");
-    const seconds = timer.querySelector("#seconds");
-    const setinterval = setInterval(updateClock,1000);
-    updateClock();
-    function updateClock(){
-      const t = getTimeRemaining(endTime);
-      days.innerHTML = t.days;
-      hours.innerHTML = t.hours;
-      minutes.innerHTML = t.minutes;
-      seconds.innerHTML = t.seconds;
-    }
-  }
-  setClock('.timer',dedLine)
-
-
-
-
-  const burger = document.querySelector('.header_menu') 
+const burger = document.querySelector('.header_menu') 
 const sidebar = document.querySelector('.sidebar')
 burger.addEventListener('click',()=>{ 
     if(burger.classList.contains('active')){ 
